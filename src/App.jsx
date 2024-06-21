a29f6db2fba2cabe75eff284e7bc459cf8cf9297
Muhammad Waqas <125763001+mwrajput@users.noreply.github.com>
2024-06-21 21:26:57 +0500

    hero banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import HeroBanner from "./components/banner/HeroBanner"; 
+import CustomCarousel from './components/Carousel/CustomCarousel';
+import AdsImage from './components/DisplayAds/AdsImage'; // Import AdsImage component
 
 const App = () => {
   return (
-    <Navbar/>
-  )
-}
+    <>
+      <Navbar />
+      <HeroBanner />
+      <div className="container container-home-desktop flex flex-col md:flex-row items-center justify-center">
+        <CustomCarousel className="w-full md:w-3/4" />
 
-export default App
+        <div className="addImage flex justify-center mt-6 md:mt-0">
+          <AdsImage
+            imageUrl="https://www.beyoung.in/api/catalog/homepage-5-dec/urban-section-deskto111p1-v1i1ew.jpg"
+            linkUrl="https://www.example.com"
+            altText="Summer Boxer"
+            title="Urban Shirts"
+            className="w-full md:w-auto"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/banner/HeroBanner.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+const HeroBanner = () => {
+  return (
+    <div className="relative z-0">
+      <Carousel 
+         autoPlay
+         infiniteLoop
+         interval={2000}  // Change slide every 3 seconds
+         showThumbs={false}
+         showStatus={false}
+         swipeable  // Allows manual dragging
+         emulateTouch  // Emulate touch event to allow dragging
+         stopOnHover  // Stop auto play on hover
+      >
+        <div className="topbanner-single">
+          <a href="https://www.beyoung.in/plain-t-shirt-combo-pick-any-4">
+            <img 
+              src="https://www.beyoung.in/api/catalog/birthday-2024/Desktop/banner/Plain-T-Shirts-banner-12desktop12-view.jpg" 
+              alt="Online shopping site for mens shirts" 
+              title="Online shopping site for mens shirts" 
+              className="w-full"
+            />
+          </a>
+        </div>
+        <div className="topbanner-single">
+          <a href="https://www.beyoung.in/mens-shirts">
+            <img 
+              src="https://www.beyoung.in/api/catalog/birthday-2024/Desktop/banner/shirt-banner-desktop-view1233.jpg" 
+              alt="Online shopping site for mens shirts" 
+              title="Online shopping site for mens shirts" 
+              className="w-full"
+            />
+          </a>
+        </div>
+        <div className="topbanner-single">
+          <a href="https://www.beyoung.in/clearance-sale">
+            <img 
+              src="https://www.beyoung.in/api/catalog/clearance/clearance-sale-banner-desktop1211.jpg" 
+              alt="Online shopping site for mens shirts" 
+              title="Online shopping site for mens shirts" 
+              className="w-full"
+            />
+          </a>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default HeroBanner;
