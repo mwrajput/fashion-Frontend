c92112afc8a3731049ce3c0baaf0f8d2ffadd2ab
H. Abdul Ghani <hafizabdulghani99@gmail.com>
2024-06-23 01:10:39 +0500

    Adding ImageSlider for Men

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import HeroBanner from "./components/banner/HeroBanner"; 
-import CustomCarousel from './components/Carousel/CustomCarousel';
-import AddvertisePIc from './components/banner/AddvertisePIc'; 
-import FooterLinks from './components/Footer/FooterLinks';
-import ForMen from './components/Header/ForMen';
-import HeadingBB from './components/Header/HeadingBB';
-import ImageSlider from './components/Imslider/ImageSlider';
-<<<<<<< Updated upstream
-import Strip from './components/StripImg/Strip';
-=======
-import ImgSliderMen from "./components/ImgSliderMen/ImgSliderMen"
+import HeroBanner from "./components/banner/HeroBanner";
+import CustomCarousel from "./components/Carousel/CustomCarousel";
+import AddvertisePIc from "./components/banner/AddvertisePIc";
+import FooterLinks from "./components/Footer/FooterLinks";
+import ForMen from "./components/Header/ForMen";
+import HeadingBB from "./components/Header/HeadingBB";
+import ImageSlider from "./components/Imslider/ImageSlider";
+import Strip from "./components/StripImg/Strip";
+import ImgSliderMen from "./components/ImgSliderMen/ImgSliderMen";
 
->>>>>>> Stashed changes
 const App = () => {
   return (
     <>
@@ -22,25 +19,24 @@ const App = () => {
       </header>
 
       <main>
-          <div className="container mb-14 mt-14 px-15">
-                  <CustomCarousel />
-                  <AddvertisePIc /> 
-                  <ForMen />
-                  <Strip />
-                  <HeadingBB />
-                  <ImageSlider />
-                  {/* <ForMen props ="Men" /> */}
-                  <ForMen data= "Men" />
-                  <HeadingBB />
-                  <ImgSliderMen />
-          </div>
-        
-        </main>
+        <div className="container mb-14 mt-14 px-15">
+          <CustomCarousel />
+          <AddvertisePIc />
+          <ForMen />
+          <Strip />
+          <HeadingBB />
+          <ImageSlider />
+          {/* <ForMen props ="Men" /> */}
+          <ForMen data="Men" />
+          <HeadingBB />
+          <ImgSliderMen />
+        </div>
+      </main>
 
       <footer>
-      <FooterLinks /> 
+        <FooterLinks />
       </footer>
-    </> 
+    </>
   );
 };
 
--- src/components/StripImg/Strip.jsx
+++ src/components/StripImg/Strip.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Strip = () => {
   return (
-    <div className="mt-18 mb-18 boder-2 border-blue">
-      <img 
-        className="lazyload fade" 
-        src="https://www.beyoung.in/api/catalog/homepage-3-10/desktop/strip/strip.jpg" 
-        title="strip" 
-        alt="Online clothing website for men and women" 
+    <div className="mt-18 mb-18">
+      <img
+        className="lazyload fade mt-10"
+        src="https://www.beyoung.in/api/catalog/homepage-3-10/desktop/strip/strip.jpg"
+        title="strip"
+        alt="Online clothing website for men and women"
       />
     </div>
   );
-}
+};
 
 export default Strip;
